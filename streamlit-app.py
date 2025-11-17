384a789bdcbeb741ddf6f9c46ed9f6fa9d72cf59
Tonttu81 <joni.pylkkonen@gmail.com>
2025-11-17 16:33:27 +0200

    Pari korjausta ja streamlit sovellus päivitetty

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,11 +1,23 @@
 import streamlit as st
+import pandas as pd
 import plotly.express as px
 
+def retrieve_temperatures_from_db():
+    conn = st.connection('mysql', type='sql')
+    query = conn.query('SELECT * FROM temperature ORDER BY date ASC;')
+    return query
+
 def main():
-    st.title("Plot some data")
+    st.title("Päivittäinen keskilämpötila Oulussa marraskuun aikana")
     
-    ff = px.scatter(x=[], y=[])
-    st.plotly_chart(ff, use_container_width=True)
+    data = retrieve_temperatures_from_db()
+
+    data_frame = pd.DataFrame(data, columns=["date", "temperature"])
+
+    scatter = px.scatter(data_frame, x='date', y='temperature')
+    st.plotly_chart(scatter, use_container_width=True)
+
+    st.write('(ei päivity automaattisesti, päivitetty viimeksi 17.11.)')
 
 if __name__ == "__main__":
     main()
