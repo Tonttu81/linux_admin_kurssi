e28b3cf46ddef216c51c4297d8c45c970a31d6c8
Tonttu81 <joni.pylkkonen@gmail.com>
2025-11-24 17:30:21 +0200

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,13 +1,42 @@
+import requests
 import streamlit as st
 import pandas as pd
+
+def retrieve_commits():
+        # retrieves amount of commits per week for the last 5 weeks from a github repository
+        url = 'https://api.github.com/repos/Tonttu81/linux_admin_kurssi/stats/participation'
+        api_token = st.secrets['github_token']
+        headers = {
+                'Authorization': 'Bearer ' + api_token,
+                'Content-Type': 'application/json',
+                'Accept': 'application/vnd.github+json'
+        }
+
+        response = requests.get(url, headers=headers)
+        response_json = response.json()
+
+        commit_counts = response_json['all']
+        commit_counts.reverse()
+        commit_counts = commit_counts[:5]
+
+        return commit_counts
 
 def retrieve_weather():
     conn = st.connection('mysql', type='sql')
     query = conn.query('SELECT * FROM weather_data ORDER BY timestamp DESC LIMIT 50;')
     return query
 
-data = retrieve_weather()
-data_frame = pd.DataFrame(data)
+weather_data = retrieve_weather()
+weather_df = pd.DataFrame(weather_data)
 
 st.title('Säädata Helsingistä')
-st.dataframe(data_frame)
+st.dataframe(weather_df)
+
+commit_data = retrieve_commits()
+commit_df = pd.DataFrame({
+        'Määrä': commit_data
+})
+commit_df.index = [f"{i}. viikkoa sitten" for i in range(len(commit_data))]
+
+st.title('Committien määrä viimeiseltä viideltä viikolta Linux kurssi Github repositorystä')
+st.dataframe(commit_df)
